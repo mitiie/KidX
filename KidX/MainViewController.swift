61e0121e767028a44438d78e9effdc442a0c8668
mitie <minhhtienn728@gmail.com>
2026-03-18 00:55:54 +0700

    inital commit

---
+++ KidX/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidX
+//
+//  Created by 𝙢𝙩 on 18/3/26.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
