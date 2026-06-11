89f775ba7a3aed7e5d15ffc9c5ac62606769becc
mitie <tienbm@leansoft.io>
2026-06-12 01:08:25 +0700

    update splash icon, button logout style

--- KidX/ViewControllers/Profile/View/ProfileController.swift
+++ KidX/ViewControllers/Profile/View/ProfileController.swift
@@ -11,6 +11,7 @@ import Lottie
 class ProfileController: BaseController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var animateView: UIView!
+    @IBOutlet weak var btnLogout: UIButton!
     
     private let viewModel: ProfileViewModel
     
@@ -36,6 +37,59 @@ class ProfileController: BaseController {
         animationView.play()
         
         self.animateView.addSubview(animationView)
+        
+        setupLogoutButton()
+    }
+    
+    private func setupLogoutButton() {
+        if #available(iOS 15.0, *) {
+            var config = UIButton.Configuration.filled()
+            config.baseForegroundColor = .white
+            config.baseBackgroundColor = UIColor(hex: 0xFF5B5B)
+            
+            // Icon config
+            config.image = UIImage(systemName: "rectangle.portrait.and.arrow.right")
+            config.imagePadding = 8
+            config.imagePlacement = .leading
+            
+            // Background corners and border
+            config.background.cornerRadius = 20
+            config.background.strokeWidth = 2.5
+            config.background.strokeColor = UIColor(hex: 0x2C2C2C)
+            
+            // Set font size
+            var titleAttr = AttributeContainer()
+            titleAttr.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+            config.attributedTitle = AttributedString("Logout", attributes: titleAttr)
+            
+            btnLogout.configuration = config
+        } else {
+            btnLogout.setTitle("Logout", for: .normal)
+            btnLogout.titleLabel?.font = UIFont.custom(15, .semiBold)
+            btnLogout.setImage(UIImage(systemName: "rectangle.portrait.and.arrow.right"), for: .normal)
+            btnLogout.backgroundColor = UIColor(hex: 0xFF5B5B)
+            btnLogout.tintColor = .white
+            btnLogout.layer.cornerRadius = 20
+            btnLogout.layer.borderWidth = 2.5
+            btnLogout.layer.borderColor = UIColor(hex: 0x2C2C2C).cgColor
+            btnLogout.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8)
+        }
+        
+        // Add press bounce target actions
+        btnLogout.addTarget(self, action: #selector(logoutButtonTouchDown), for: .touchDown)
+        btnLogout.addTarget(self, action: #selector(logoutButtonTouchUp), for: [.touchUpInside, .touchUpOutside, .touchCancel])
+    }
+    
+    @objc private func logoutButtonTouchDown(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: [.allowUserInteraction, .curveEaseOut], animations: {
+            sender.transform = CGAffineTransform(scaleX: 0.94, y: 0.94)
+        }, completion: nil)
+    }
+    
+    @objc private func logoutButtonTouchUp(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.15, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: [.allowUserInteraction, .curveEaseOut], animations: {
+            sender.transform = .identity
+        }, completion: nil)
     }
 
     private func setupTableView() {
